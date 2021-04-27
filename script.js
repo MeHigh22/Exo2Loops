43c0034d8ed8f68bf023aadd0b5423d71329a77e
MeHigh22 <pmihai31@gmail.com>
2021-04-27 09:27:53 +0200

    Push

---
+++ script.js
@@ -0,0 +1,14 @@
+let eleves = ["Alexis", "Andy", "Charles", "Dorian", "Farhad", "Ilyas", "Junior", "Kev", "Loïc", "Mihai", "Oussama", "Tania", "William", "Alain"];
+
+
+//Donc ce qu'on va faire, on déclare une variable qui peut prendre n'importe quel nom, en occurence e, après pour chaque e on va faire une boucle.
+eleves.forEach(e => {
+    console.log(`Bonjour ${e}`)
+})
+
+let array = ["Serviette","TarpinBien","SavonDeMarseille","HuileDeCOco","HuileD'oregan"]
+
+array.forEach(e => {
+    console.log(`Bonjour ${e}`)
+})
+
